c482ee0f8085bef35690ce148a13ffcc8aa6b7c2
Hassan Yahya <hassanyahya400@gmail.com>
2024-11-10 18:39:10 +0100

    ft: add service card and service image card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,25 +1,20 @@
-import { nextIcon } from "../assets/icons"
-import Button from "../components/Button"
-import ImagesComponent from "./ImagesComponent"
+import { nextIcon } from "../assets/icons";
+import Button from "../components/Button";
+import ImagesComponent from "./ImagesComponent";
+import { MdOutlineNavigateNext } from "react-icons/md";
 
-const ServiceCard = ({title, description, className="", showButton = true}) => {
+const ServiceCard = ({ title, description, showButton = true }) => {
   return (
-   <div className="bg-tt-white w-48 p-4">
-    <div className="flex flex-col lg:flex-col">
-      <h1 className="text-tt-primary text-sm mb-4">{title}</h1>
-      <p className={`text-tt-grey text-sm mb-2 ${className} `}>{description}</p>
+    <div className="shadow-sm rounded-lg p-6 max-w-sm mx-auto">
+      <h2 className="text-2xl font-semibold text-tt-primary mb-4">{title}</h2>
+      <p className="text-tt-black mb-4">{description}</p>
+      <Button
+        type="outline"
+        text="Learn More"
+        icon={<MdOutlineNavigateNext />}
+      />
     </div>
-    {
-      showButton && (
-        <Button text="Learn More" type="outline"  
-    icon={<ImagesComponent src={nextIcon} className="w-5 h-5" />}
-     />
-      )
-    }
-    
-   </div>
+  );
+};
 
-  )
-}
-
-export default ServiceCard
+export default ServiceCard;
---
+++ src/components/ServiceWithImageCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "./Button";
+import { MdOutlineNavigateNext } from "react-icons/md";
+
+const ServiceWithImageCard = ({ title, description, body, linkTo, img }) => {
+  return (
+    <div className="flex bg-white shadow-lg rounded-lg p-6 max-w-4xl mx-auto">
+      <div className="flex-1 pr-6">
+        <p className="text-tt-primary font-semibold mb-4">{title}</p>
+        <h2 className="text-2xl font-semibold text-tt-black mb-2">
+          {description}
+        </h2>
+        <p className="text-tt-black mb-4">{body}</p>
+        <a href={linkTo}>
+          <Button
+            type="outline"
+            text="Learn More"
+            icon={<MdOutlineNavigateNext />}
+          />
+        </a>
+      </div>
+
+      <div className="flex-shrink-0">
+        <img
+          src={img}
+          alt={title}
+          className="rounded-lg w-full h-auto max-w-sm object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ServiceWithImageCard;

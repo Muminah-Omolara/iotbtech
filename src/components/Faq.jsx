f47763b7741cd65df24898cd88c56cc7372e72e0
Oriadelatifat <163722234+Oriadelatifat@users.noreply.github.com>
2024-11-10 22:06:38 +0100

    navbar-footer-faq-component-created

---
+++ src/components/Faq.jsx
@@ -0,0 +1,29 @@
+import FaqItem from "./Faqitem";
+
+const Faq = () => {
+    return <div class="h-[661.25px] lg:mx-auto mx-4 flex flex-col lg:flex-row w-auto max-w-5xl gap-12 ">
+        <div class=" ">
+            <div class=" mt-[200px]">
+                <p class=" text-tt-primary font-[700] text-[22px]">FAQ</p>
+                <p class="font-[700] text-[40px] text-tt-black leading-[60px] mt-[30px]">Here are Some Answer For  Frequntly asked Questions</p>
+                <p class=" font-[400] text-[22px] text-tt-grey mt-[30px]">We got alot of Message With the same Questions so here are some fast answer for populare Question</p>
+
+            </div>
+        </div>
+        <div class=" text-tt-primary lg:mt-36 mt-4">
+            <div ><FaqItem question=" Do You offer Software Development" answer=" yes,we do"/></div>
+            <FaqItem question=" Do you offer Social Media Marketing" answer="Yes,we do offer Social Media marketing , we offer fb ads instgram ads ,google campagine , tiktok promotion ,and other social media plateforme  "/>
+            <FaqItem question=" Do you Offer pack Services" answer=" No, we do not offer psck services"/>
+            <FaqItem question=" How long it take to realise a projrct" answer=" 500 years "/>
+        </div>
+
+    </div>;
+  };
+  
+
+export default Faq
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,16 @@ export default {
       colors: {
         "tt-primary": "#008080",
         "tt-black": "#0E0E2C",
-        "tt-grey": "#A0A4AB"
-      }
+        "tt-grey": "#A0A4AB",
+        "tt-white": "#FFFFFF"
+      },
+        customUtilities:{
+          ".text-h1":{
+          fontSize:"16px",
+          fontWeight:500,
+          lineHeight:"24px",
+          color:"#FFFFFF",}
+        },
     },
   },
   plugins: [],

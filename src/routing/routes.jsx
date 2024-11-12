5f2aaf1f013b60754bfcc75df424c06a628b6cbf
Waliyat <waliyatkehinde@gmail.com>
2024-11-12 18:21:35 +0100

    services-offer section refactor

--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -13,13 +13,13 @@ import Services from "../pages/Services";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />} />
+    <Route path="/" >
+      <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<Services />} />
-    </>,
+    </Route>,
   ),
 );
 

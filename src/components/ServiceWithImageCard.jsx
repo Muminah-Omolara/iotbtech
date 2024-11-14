467ff698e8aa3adaa14e0ba2584de1c62e0ef1cb
Waliyat <waliyatkehinde@gmail.com>
2024-11-14 09:22:10 +0100

    worked on the mobile view of portfolio page

--- src/components/ServiceWithImageCard.jsx
+++ src/components/ServiceWithImageCard.jsx
@@ -4,7 +4,7 @@ import { MdOutlineNavigateNext } from "react-icons/md";
 // eslint-disable-next-line react/prop-types
 const ServiceWithImageCard = ({ title, description, body, linkTo, img }) => {
   return (
-    <div className="flex bg-white shadow-lg rounded-lg p-6 max-w-4xl mx-auto">
+    <div className="flex flex-col lg:flex-row md:flex-row bg-white shadow-lg rounded-lg p-6 max-w-4xl mx-auto">
       <div className="flex-1 pr-6">
         <p className="text-tt-primary font-semibold mb-4">{title}</p>
         <h2 className="text-2xl font-semibold text-tt-black mb-2">
@@ -24,7 +24,7 @@ const ServiceWithImageCard = ({ title, description, body, linkTo, img }) => {
         <img
           src={img}
           alt={title}
-          className="rounded-lg w-full h-auto max-w-sm object-cover"
+          className="rounded-lg w-full h-auto max-w-sm object-cover mt-5 lg:mt-0"
         />
       </div>
     </div>

92af140e4c7047bab9e870e32e88eaf5468aa24a
Hassan Yahya <hassanyahya400@gmail.com>
2024-11-17 23:32:30 +0100

    add back icon on 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { IoArrowBackOutline } from "react-icons/io5";
 
 const NotFound = () => {
   return (
@@ -15,10 +16,8 @@ const NotFound = () => {
                     Sorry, the page you are looking for could not be found or has been removed.
                 </p>
                 <Link to="/" className="text-tt-primary duration-150 font-medium inline-flex items-center gap-x-1">
-                    Go back
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                        <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
-                    </svg>
+                    <IoArrowBackOutline />
+                   <span> Go back</span>
                 </Link>
             </div>
         </div>

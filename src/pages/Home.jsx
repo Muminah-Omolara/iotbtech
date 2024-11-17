6d60d193ba875c15ab24a21d921dbb68149a28f4
Hassan Yahya <hassanyahya400@gmail.com>
2024-11-17 11:35:45 +0100

    fix issues with reg page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import TeamCard from "../components/TeamCard";
-import Login from "./Authentication/Login";
 import Clients from "../sections/Clients";
 import HomeHero from "../sections/HomeHero";
 import ServicesOffer from "../sections/ServicesOffer";

8396cf0b18c925b35c793686c83a42d059d5a3c5
Olaiday1 <hammedahishat7@gmail.com>
2024-12-01 18:03:31 +0100

    Worked on the service image component

--- src/sections/ServiceWithImage.jsx
+++ src/sections/ServiceWithImage.jsx
@@ -4,8 +4,8 @@ import OurServiceWithImage from "../components/OurServiceWithImage";
 const ServiceWithImage = () => {
   return (
     <>
-      <div className="flex flex-row justify-start lg:my-10 mb-24 mx-4 lg:justify-between lg:items-center ">
-        <div className="flex flex-col lg:flex-row lg:mx-24 items-left">
+      <div className="flex flex-row justify-center lg:my-10 mb-24 mx-4 lg:justify-center lg:items-center ">
+        <div className="flex flex-col lg:flex-row lg:mx-20 items-left">
           <OurServiceWithImage
             title="Ui /Ux , Web Design"
             description="With a team of the best Digital Product Designer In the world at your hand , their goal is to create usefull andeasy to use products for you potentials users"

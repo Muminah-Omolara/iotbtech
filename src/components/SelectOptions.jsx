1a2b2152d939ad988e8606a0e747b60b10db4447
Waliyat <waliyatkehinde@gmail.com>
2024-12-25 11:42:14 +0100

    update the fellow registration page

--- src/components/SelectOptions.jsx
+++ src/components/SelectOptions.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable react/prop-types */
 
-// eslint-disable-next-line react/prop-types
-const SelectOptions = ({ label, options, required }) => {
+const SelectOptions = ({ label, options, required, onChange, name }) => {
   return (
     <div>
         {label && (
@@ -8,10 +8,12 @@ const SelectOptions = ({ label, options, required }) => {
                 {label} {required && "*"}
             </label>
         )}
-        <select className="w-full mt-2 px-3 py-2 outline-none border focus:border-tt-primary shadow-sm rounded-lg" required={required}>
+        <select className="w-full mt-2 px-3 py-2 outline-none border focus:border-tt-primary shadow-sm rounded-lg" required={required}
+            onChange={onChange} name={name}
+        >
             <option value={options} className="text-tt-black">Select</option>
             {options.map((opt) => (
-                <option key={opt} value={opt.value}>
+                <option key={opt.value} value={opt.value}>
                     {opt.label}
                 </option>
             ))}
--- src/pages/auth/FellowRegistration.jsx
+++ src/pages/auth/FellowRegistration.jsx
@@ -1,14 +1,35 @@
 import CustomInput from "../../components/CustomInput"
-import NigeriaStates from "../../components/NigeriaStates"
 import SelectOptions from "../../components/SelectOptions"
-import cohortList from "../../data/cohortList"
 import genderOptions from "../../data/genderOptions"
 import stackLists from "../../data/stackLists"
 import line from "../../assets/images/line.svg";
 import { iotbTech } from "../../assets/images"
 import Button from "../../components/Button"
+import { useState } from "react"
+import stateLists from "../../data/StateLists";
 
 const FellowRegistration = () => {
+    const [ formData, setFormData ] = useState({
+        firstName: "",
+        lastName: "",
+        otherName: "",
+        email: "",
+        phoneNumber: "",
+        altPhoneNumber: "",
+        dob: "",
+        yearOfExperience: "",
+        priorTechExperience: "",
+        cohort: "",
+        gender: "Male"
+    })
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevFormData) => ({...prevFormData, [name]: value}))
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Button Clicked", formData)
+    }
   return (
     <div className="mx-auto leading-snug max-w-screen-xl px-4 sm:px-6 lg:px-8 py-10">
         <img src={line} alt="" className="absolute w-full -left-10 -right-10 -bottom-44 -z-10 hidden md:block" />
@@ -21,67 +42,103 @@ const FellowRegistration = () => {
                     We will email your form print-out after submission.
                 </p>
             </div>
-            <form className="space-y-4">
+            <form onSubmit={handleSubmit} className="space-y-4">
                 <h3 className="text-2xl text-tt-black">Personal Details</h3>
                 <CustomInput 
                     type="text"
                     placeholder="Enter your first name"
                     label="First Name"
-                    name="first-name"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleInputChange}
+                    required={true}
                 />
                 <CustomInput 
                     type="text"
                     placeholder="Enter your last name"
                     label="Last Name"
-                    name="last-name"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleInputChange}
+                    required={true}
                 />
                 <CustomInput 
                     type="text"
                     placeholder="Enter your other name"
                     label="Other Name"
-                    name="other name"
+                    name="otherName"
+                    value={formData.otherName}
+                    onChange={handleInputChange}
+                    required={false}
                 />
                 <CustomInput 
                     type="email"
                     placeholder="iotbtech@example.com"
                     label="Email"
                     name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required={true}
                 />
                 <CustomInput 
                     type="number"
                     placeholder="2349060609721"
                     label="Phone Number"
-                    name="Phone Number"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleInputChange}
+                    required={true}
                 />
                     <CustomInput 
                     type="number"
                     placeholder="2349060609721"
                     label="Alternate Phone Number"
-                    name="Alternate Phone Number"
+                    name="altPhoneNumber"
+                    value={formData.altPhoneNumber}
+                    onChange={handleInputChange}
+                    required={false}
                 />
-                <SelectOptions label="Gender" options={genderOptions}/>
+                <SelectOptions label="Gender" options={genderOptions} onChange={handleInputChange} name="gender"/>
                 <CustomInput 
                     type="date"
                     placeholder="DD/MM/YYYY"
                     label="Date of Birth"
-                    name="Date of Birth"
+                    name="dob"
+                    value={formData.dob}
+                    onChange={handleInputChange}
+                    required={false}
                 />
-                <NigeriaStates label="States" />
+                <SelectOptions label="States" options={stateLists} onChange={handleInputChange} name="states" />
                 <h3 className="text-2xl text-tt-black">Occupations</h3>
                 <CustomInput 
                     type="number"
                     placeholder="Enter your answer"
-                    name="Years of Experience"
+                    label="Years of Experience"
+                    name="yearOfExperience"
+                    value={formData.yearOfExperience}
+                    onChange={handleInputChange}
+                    required={true}
                 />
                 <CustomInput 
                     type="text"
                     placeholder="Enter your answer"
                     label="Prior Tech Experience"
-                    name="Prior Tech Experience"
+                    name="priorTechExperience"
+                    value={formData.priorTechExperience}
+                    onChange={handleInputChange}
+                    required={true}
                 />
                 <h3 className="text-2xl text-tt-black">Application Details</h3>
                 <SelectOptions label="Stack/Programme" options={stackLists} />
-                <SelectOptions label="Cohort" options={cohortList} />
+                <CustomInput 
+                    type="text"
+                    placeholder="Cohort"
+                    label="Cohort"
+                    name="cohort"
+                    value={formData.cohort}
+                    onChange={handleInputChange}
+                    required={true}
+                />
 
                 <div className="flex justify-center items-center">
                     <Button text="Submit" />

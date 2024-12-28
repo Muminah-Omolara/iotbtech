8147184aca1c2c8717df8592abb190dd2d6eb43e
ohinoyi11 <ohinoyiobanyi11@gmail.com>
2024-12-28 12:55:17 +0100

    update logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/icons/tektariq.svg";
+import Logo from "../assets/icons/iotbtech.svg";
 
 const TheLogo = () => {
   return <img src={Logo} alt="tektariq-logo" className="max-w-24" />;

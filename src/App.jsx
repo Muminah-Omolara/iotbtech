7a501a2c5c59d6121ec5b26ba6586675ac492c05
Almajiri Dev <almajiri.dev@gmail.com>
2025-01-16 22:28:13 +0100

    removed unnecassry imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+// import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
   return (
